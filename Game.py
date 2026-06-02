4ad47acf685a05cb4a777dc6498017431edefefa
Gray <161078380+Grayt2@users.noreply.github.com>
2026-06-02 12:48:40 -0400

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,16 +1,19 @@
 import time
-global time
-time = time.time()
-class save_game:
-    global time
-    try:
-        f = open("/home/lumi/Documents/Python_Game/Cash", "x")
-    except:
-        f = open("/home/lumi/Documents/Python_Game/Cash", "a")
-        f.write(f"")
-
+cash = 10
+save_data = str(input("Enter save string: "))
 def Game():
-    class upgrade_1:
-        def __init__(self, number_of_upgrades):
-            self.number_of_upgrades = number_of_upgrades
-        
+  time_atstart = time.time()
+  global cash
+  class upgrade_1:
+    def __init__(self, number_of_upgrades, cash):
+      self.cash = cash
+      self.number_of_upgrades = number_of_upgrades
+    def formula(self):
+      self.cash = self.cash * ((self.number_of_upgrades/6)+1)
+      return self.cash
+  p = upgrade_1(1, cash)
+  cash = p.formula()
+  return cash
+print(Game())
+  
+  
